bb63e28611ccb5a170948bd6000eee403e2b814d
Ezz <izzabuassab@hotmail.com>
2021-09-11 00:43:01 +0300

    added enteredSum and its display

--- src/com/company/VMRunnder.java
+++ src/com/company/VMRunnder.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class VMRunnder {
     private VendingMachine vm;
+    private int selectedProd;
+    private HashMap<Coin, Integer> enteredCoins;
+    private int enteredSum;
 
     public VMRunnder(VendingMachine vm) {
         this.vm = vm;
+        this.enteredCoins = new HashMap<>();
+        this.enteredSum = 0;
     }
 
     public VendingMachine getVm() {
@@ -13,5 +21,35 @@ public class VMRunnder {
 
     public void setVm(VendingMachine vm) {
         this.vm = vm;
+    }
+
+    public int getSelectedProd() {
+        return selectedProd;
+    }
+
+    public void setSelectedProd(int selectedProd) {
+        this.selectedProd = selectedProd;
+    }
+
+    public HashMap<Coin, Integer> getEnteredCoins() {
+        return enteredCoins;
+    }
+
+    public void setEnteredCoins(HashMap<Coin, Integer> enteredCoins) {
+        this.enteredCoins = enteredCoins;
+    }
+
+    public int getEnteredSum() {
+        return enteredSum;
+    }
+
+    public void setEnteredSum(int enteredSum) {
+        this.enteredSum = enteredSum;
+    }
+
+    public void displayBalance(){
+        System.out.println(" The current entered amount is: " + this.getEnteredSum());
+        System.out.println(" ------------------------------------------ ");
+
     }
 }
